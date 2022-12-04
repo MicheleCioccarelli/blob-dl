ded7c320fb1eb0bfa7f96a9364dd353d6a17078e
Michele <cioccarellimi@gmail.com>
2022-12-04 15:46:11 +0100

    Initial setting up of command parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = "Long about goes brr")]
+//#[command(next_line_help = true)]
+struct Cli {
+    /// Link to the video/playlist you want to download
+    url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("url: {}", cli.url);
 }
