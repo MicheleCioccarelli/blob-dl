64d91d3d34d79ed7ac464b41206682250c64acc9
Michele <cioccarellimi@gmail.com>
2023-01-06 16:09:52 +0100

    Refactored assembling::from_command() to use '?' instead of a million match statements and updated its documentation

--- src/assembling.rs
+++ src/assembling.rs
@@ -83,35 +83,37 @@ mod youtube {
         is_best: bool,
     }
     impl Format {
-        /// # TODO: Fix error handling
         /// # Usa insiemistica (intersezione tra insiemi di id) e mappe per comparare
         /// Returns an Option\<Format\> object when given a line from the output of the command
         /// "youtube-dl -F \<URL\>"
+        /// # Contract
+        /// This function returns `None` if it encounters any problems, it is up to the caller
+        /// to check for them if they want to do error-handling.
+        ///
+        /// These conditions are listed in the `Returns None` section
         /// # Returns Some(Format)
         /// When `ytdl_output_line` contains information about (audio-only or video) quality and format for a youtube url
         ///
         /// # Returns None
-        /// If `ytdl_output_line` isn't about video quality and format
+        /// When `ytdl_output_line` isn't about video quality and format
         /// (for example lines starting with \[info\] or \[youtube\])
         ///
-        /// or when `ytdl_output_line` is about a video-only format
-        pub(crate) fn from_command(ytdl_output_line: &str) -> Option<Format> {
+        /// When `ytdl_output_line` is about a video-only format
+        ///
+        /// When `ytdl_output_line` is an error/warning (these lines start with `ERROR` or `WARNING`)
+        pub fn from_command(ytdl_output_line: &str) -> Option<Format> {
             // Skip lines without useful format information
             if ytdl_output_line.contains("[") ||
                 ytdl_output_line.contains("format") ||
                 ytdl_output_line.contains("video only") ||
                 ytdl_output_line.contains("ERROR") {
-                // TODO Hande this type of error
                 return None;
             };
-
             // Collect all elements in a line in a single vector
             let table_elements: Vec<&str> = ytdl_output_line.split_whitespace().collect();
-
             // 8 is the minimum amount of fields in a valid output
             if table_elements.len() < 8 {
                 eprintln!("This youtube-dl output line was rejected: {}", ytdl_output_line);
-                // TODO more error handling 🎉
                 return None;
             }
             let mut table_elements_iter = table_elements.into_iter();
@@ -122,22 +124,14 @@ mod youtube {
             * 22           mp4        1280x720   720p  468k , avc1.64001F, 30fps, mp4a.40.2 (44100Hz) (best)
             * 140          m4a        audio only tiny  127k , m4a_dash container, mp4a.40.2@127k (44100Hz), 6.54MiB
             *
-            * The fields are: code, extension, resolution/audio-only, quality, note, size, ..., (best)[optional]
+            * The fields are: code, extension, resolution/audio-only, quality, note, size, ..., (best)
             */
-            let code: u32 = match table_elements_iter.next() {
-                Some(code_) => code_.parse().expect("Problem parsing format code"),
-                None => panic!("Previous length check ensured this element would be available"),
-            };
 
-            let file_extension = match table_elements_iter.next() {
-                Some(file_extension_) => String::from(file_extension_),
-                None => panic!("Previous length check ensured this element would be available"),
-            };
+            let code: u32 = table_elements_iter.next()?.parse().ok()?;
 
-            let resolution = match table_elements_iter.next() {
-                Some(resolution_) => String::from(resolution_),
-                None => panic!("Previous length check ensured this element would be available"),
-            };
+            let file_extension = String::from(table_elements_iter.next()?);
+
+            let resolution = String::from(table_elements_iter.next()?);
 
             // Audio only files' resolution is marked as "audio only", video files have an actual resolution
             let audio_only = if resolution == "audio" {
@@ -148,43 +142,34 @@ mod youtube {
                 false
             };
 
-            // Skip the "note" section of the line
+            // Skip the "note" section of ytdl_output_line
             table_elements_iter.next();
 
-            let size = match table_elements_iter.next() {
-                Some(mut size_) => {
-                    if audio_only {
-                        // Audio-only with DASH note has one more field to be skipped
-                        if size_ == "audio" {
-                            size_ = match table_elements_iter.next() {
-                                Some(s) => s,
-                                None => panic!("Length checks prevent this case"),
-                            }
-                        }
-                    }
-                    String::from(size_)
-                },
-                None => panic!("Previous length check ensured this element would be available"),
-            };
-
-            if let Some(last_element) = table_elements_iter.last() {
-                let is_best = if last_element == "(best)" {
-                    true
-                } else {
-                    false
-                };
-                // All information has been parsed
-                Some(Format {
-                    code: code,
-                    file_extension: file_extension,
-                    resolution: resolution,
-                    size: size,
-                    audio_only: audio_only,
-                    is_best: is_best,
-                })
-            } else {
-                panic!("Last element should always be available due to previous length checks");
+            let mut size = String::from(table_elements_iter.next()?);
+            if audio_only {
+                // Audio-only with DASH note has one more field to be skipped
+                if size == "audio" {
+                    size = String::from(table_elements_iter.next()?);
+                }
             }
+
+            let last_element = table_elements_iter.last()?;
+
+            // The last element of ytdl_output_line tells you whether this line had the best available format
+            let is_best = if last_element == "(best)" {
+                true
+            } else {
+                false
+            };
+            // All information has been parsed
+            Some(Format {
+                code: code,
+                file_extension: file_extension,
+                resolution: resolution,
+                size: size,
+                audio_only: audio_only,
+                is_best: is_best,
+            })
         }
 
         fn code(&self) -> u32 {
