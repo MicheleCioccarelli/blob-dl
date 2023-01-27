1a1cd4bec3000ac08386d91334ef3de51b83a9a6
Michele <cioccarellimi@gmail.com>
2023-01-27 22:57:07 +0100

    Implemented youtube playlist command generation

--- src/assembling/youtube/yt_playlist/config.rs
+++ src/assembling/youtube/yt_playlist/config.rs
@@ -30,59 +30,70 @@ impl<'a> YtPlaylistConfig<'a> {
 
     /// Builds a yt-dl command with the needed specifications (downloads a playlist)
     pub(crate) fn build_command(&self) -> std::process::Command {
-        panic!("Command building is currently broken")
-        // let mut command = std::process::Command::new("youtube-dl");
-        //
-        // // Continue even when errors are encountered
-        // command.arg("-i");
-        //
-        // // Setup output directory and naming scheme
-        // command.arg("-o");
-        // command.arg(
-        //     {
-        //         let mut path_and_scheme = String::new();
-        //
-        //         // Add the user's output path (empty string for current directory)
-        //         path_and_scheme.push_str(self.output_path.as_str());
-        //
-        //         // Create a directory named after the playlist
-        //         path_and_scheme.push_str("/%(playlist)s/");
-        //
-        //         if self.include_indexes {
-        //             path_and_scheme.push_str("%(playlist_index)s_");
-        //         }
-        //
-        //         // Add the video's title to the file name
-        //         path_and_scheme.push_str("%(title)s");
-        //         // Add the correct file extension
-        //         path_and_scheme.push_str(".%(ext)s");
-        //         path_and_scheme
-        //     });
-        //
-        // // Quality and format selection
-        // match self.media_selected {
-        //     assembling::MediaSelection::Video => {
-        //         command.arg("-f");
-        //         command.arg(
-        //             {
-        //                 let mut quality_format = match self.quality {
-        //                     //Quality::Bestquality => String::from("best"),
-        //                     //Quality::Worstquality => String::from("worst"),
-        //                 };
-        //
-        //                 // Add file format
-        //                 quality_format.push_str("[ext=");
-        //                 quality_format.push_str(self.download_format.as_str());
-        //                 quality_format.push_str("]");
-        //                 quality_format
-        //             });
-        //     },
-        //     assembling::MediaSelection::Audio => (),
-        // };
-        //
-        // // Add the playlist's url
-        // command.arg(&self.url);
-        //
-        // command
+        let mut command = std::process::Command::new("youtube-dl");
+
+        // Continue even when errors are encountered
+        command.arg("-i");
+
+        // Setup output directory and naming scheme
+        command.arg("-o");
+        command.arg(
+            {
+                let mut path_and_scheme = String::new();
+
+                // Add the user's output path (empty string for current directory)
+                path_and_scheme.push_str(self.output_path.as_str());
+
+                // Create a directory named after the playlist
+                path_and_scheme.push_str("/%(playlist)s/");
+
+                if self.include_indexes {
+                    path_and_scheme.push_str("%(playlist_index)s_");
+                }
+
+                // Add the video's title to the file name
+                path_and_scheme.push_str("%(title)s");
+                // Add the correct file extension
+                path_and_scheme.push_str(".%(ext)s");
+                path_and_scheme
+            });
+
+        // Makes the id live long enough to be used as an arg for command.
+        // If it was fetched from the next match arm the temporary &str would not outlive command
+        let id = match self.chosen_format {
+            VideoQualityAndFormatPreferences::UniqueFormat(id) => id.to_string(),
+            _ => String::new(),
+        };
+
+        // Quality and format selection
+        command.arg("-f");
+        match self.media_selected {
+            MediaSelection::Video => {
+                command.arg(
+                    {
+                        match self.chosen_format {
+                            VideoQualityAndFormatPreferences::BestQuality => "best",
+                            VideoQualityAndFormatPreferences::WorstQuality => "worst",
+                            VideoQualityAndFormatPreferences::UniqueFormat(_) => id.as_str(),
+                        }
+                    });
+            },
+
+            MediaSelection::Audio => {
+                command.arg(
+                    {
+                        match self.chosen_format {
+                            VideoQualityAndFormatPreferences::BestQuality => "bestaudio",
+                            VideoQualityAndFormatPreferences::WorstQuality => "worstaudio",
+                            VideoQualityAndFormatPreferences::UniqueFormat(_) => id.as_str(),
+                        }
+                    });
+            },
+        };
+
+        // Add the playlist's url
+        command.arg(self.url);
+
+        command
     }
 }
