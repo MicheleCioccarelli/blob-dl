4ea05bb5f61d0d1aace2d7e9f3bb1b578bb7cdce
michele <cioccarellimi@gmail.com>
2023-08-06 15:45:37 +0200

    Small prompt changes

--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,7 @@ use crate::error::{BlobdlError, BlobResult};
 
 pub fn parse_config() -> BlobResult<CliConfig> {
     let matches = Command::new("blob-dl")
-        .version("2023.07.13")
+        .version("0.1.0")
         .author("cioccarellimi@gmail.com")
         .about(SHORT_ABOUT)
         .long_about(LONG_ABOUT)
